7a8be8afa0f2b1de0cfe02817b3bf69b6e2298e2
aszadzinski <albert.szadzinski@smcebi.edu.pl>
2019-12-20 15:39:40 +0100

    Fixed: db import problem

--- App/__init__.py
+++ App/__init__.py
@@ -1,5 +1,3 @@
-from App import models
-from App import views
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -8,3 +6,6 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+from App import models
+from App import views
