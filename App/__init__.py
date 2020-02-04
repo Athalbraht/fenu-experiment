c275857e38ef612677a60111d970499a4f03e190
aszadzinski <albert.szadzinski@smcebi.edu.pl>
2020-02-04 10:09:11 +0100

    Updated: dashboard menu: BETA section

--- App/__init__.py
+++ App/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -6,6 +7,8 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+log = logging.getLogger('werkzeug')
+log.disabled = True
 
 from App import models
 from App import views
