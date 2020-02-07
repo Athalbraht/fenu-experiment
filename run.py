4e003ec186c641c61191c41da29c106cb3b7d0c7
aszadzinski <albert.szadzinski@smcebi.edu.pl>
2020-02-07 16:21:23 +0100

    Hyperdash support

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from App import app
 import config
 
+from hyperdash import Experiment
+
+exp = Experiment("fenu-exp.pl:h235")
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="8000")
