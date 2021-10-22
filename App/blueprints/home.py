38fa412bf3c8ed9af92536572e733b1caf0c2d5e
aszadzinski <albert.szadzinski@us.edu.pl>
2021-10-22 13:59:30 +0200

    Added: base themes part 1

--- App/blueprints/home.py
+++ App/blueprints/home.py
@@ -2,12 +2,9 @@ from .misc import *
 
 @app.route('/home')
 def home_page():
-    return render_template('main.html')
+    return render_template('overview.html')
 
-@app.route('/dashboard')
-def dashboard_page():
-    return render_template('dashboard.html')
 
 @app.route('/xD')
 def dashboard_pagexd():
-    return render_template('dashboard2.html')
+    return render_template('dashboard/overview.html')
